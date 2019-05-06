aae56fc2be8c09511d763af8730dd1ad29116fdd
Jimmy Chicaiza <jimmy.chicaiza@gmail.com>
2019-05-06 18:14:55 -0400

    new changes

---
+++ getandprint.js
@@ -0,0 +1,13 @@
+function getAndPrintHTML () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step2.html'
+    };
+  
+    /* Add your code here */
+
+   var = ;   //new var for append each chunk of data
+
+
+  }
